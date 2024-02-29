3c00d887759a5c832375ac86fe4e19c323d90c8b
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-02-29 12:20:58 +0700

    View dan ViewGroup

---
+++ app/src/main/java/com/fajri/bangkit_2024_bmaaup/ViewDanViewGroup/ConstraintLayout.kt
@@ -0,0 +1,12 @@
+package com.fajri.bangkit_2024_bmaaup.ViewDanViewGroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fajri.bangkit_2024_bmaaup.R
+
+class ConstraintLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
---
+++ app/src/main/java/com/fajri/bangkit_2024_bmaaup/ViewDanViewGroup/ViewDanViewGroup.kt
@@ -0,0 +1,12 @@
+package com.fajri.bangkit_2024_bmaaup.ViewDanViewGroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fajri.bangkit_2024_bmaaup.R
+
+class ViewDanViewGroup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_dan_view_group)
+    }
+}
