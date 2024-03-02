dfd581a187733c1b11737d43344b7ac8f51eea4e
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2024-03-02 19:19:23 +0700

    RecyclerView Glide

--- app/src/main/java/com/fajri/bangkit_2024_bmaaup/RecyclerView/ListHeroAdapter.kt
+++ app/src/main/java/com/fajri/bangkit_2024_bmaaup/RecyclerView/ListHeroAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.fajri.bangkit_2024_bmaaup.R
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
@@ -31,7 +32,11 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>): RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val (name, description, photo) = listHero[position]
-        holder.imgPhoto.setImageResource(photo)
+//        holder.imgPhoto.setImageResource(photo)
+        // Menggunakan Glide
+        Glide.with(holder.itemView.context)
+            .load(photo) // URL Gambar
+            .into(holder.imgPhoto) // imageView mana yang akan diterapkan
         holder.tvName.text = name
         holder.tvDescription.text = description
 
